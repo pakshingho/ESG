e4df67f42912335ab6a2287c64d53ac623ee4440
Pak Shing Ho <psho@bu.edu>
2019-11-02 23:16:15 -0400

    KLD CRSP linked by CUSIP and by TICKER

--- KLD_CRSP_Link.py
+++ KLD_CRSP_Link.py
@@ -35,13 +35,13 @@ _kld2['cusip'].replace({'NA':None, '0':None, '#N/A':None}, inplace=True)
 _kld2['ticker'].replace({'NA':None, '#N/A':None}, inplace=True)
 _kld2['companyname'] = _kld2['companyname'].str.upper()
 
-# Back fill and forward fill missing CUSIPs
+# Back fill and forward fill missing CUSIPs. Can also try bfill ticker.
 _kld2['cusip'] = _kld2.groupby(['companyname'])['cusip'].bfill().ffill()
 
 # Construct dates pre-2000, month is Aug; from 2001, monnth is Dec, all days are 31.
 _kld2['month'] = '12'
 _kld2['day'] = '31'
-_kld2.loc[_kld2.year<=2000, 'month'] = '08'
+_kld2.loc[_kld2.year<=2000, 'month'] = '08' # commented out this has no effect on linking
 _kld2.year = _kld2.year.astype(int).astype(str)
 _kld2['date'] = pd.to_datetime(_kld2[['year', 'month', 'day']]).dt.date
 _kld2.drop(columns=['month', 'day'], inplace=True)
@@ -50,12 +50,12 @@ _kld2_date = _kld2.groupby(['companyname','cusip']).date.agg(['min', 'max'])\
 .reset_index().rename(columns={'min':'fdate', 'max':'ldate'})
 
 # merge fdate ldate back to _kld2 data
-_kld2 = pd.merge(_kld2, _kld2_date,how='left', on =['companyname','cusip'])
-_kld2 = _kld2.sort_values(by=['companyname','cusip','date'])
+_kld3 = pd.merge(_kld2, _kld2_date,how='left', on =['companyname','cusip'])
+_kld3 = _kld3.sort_values(by=['companyname','cusip','date'])
 
 # keep only the most recent company name
 # determined by having date = ldate
-_kld2 = _kld2.loc[_kld2.date == _kld2.ldate].drop(['date'], axis=1)
+_kld3 = _kld3.loc[_kld3.date == _kld3.ldate].drop(['date'], axis=1)
 
 
 # 1.2 CRSP: Get all permno-ncusip combinations
@@ -86,7 +86,7 @@ _crsp2 = _crsp2.loc[_crsp2.enddt ==_crsp2.nameenddt].drop(['enddt'], axis=1)
 # 1.3 Create CUSIP Link Table
 
 # Link by full cusip, company names and dates
-_link1_1 = pd.merge(_kld2, _crsp2, how='inner', left_on='cusip', right_on='ncusip')\
+_link1_1 = pd.merge(_kld3, _crsp2, how='inner', left_on='cusip', right_on='ncusip')\
 .sort_values(['companyname','permno','ldate'])
 
 # Keep link with most recent company name
@@ -137,3 +137,98 @@ def score1(row):
 _link1_2['score']=_link1_2.apply(score1, axis=1)
 _link1_2 = _link1_2[['cusip_x', 'ticker','permno','companyname','comnam','name_ratio','score']].rename(columns={'cusip_x':'cusip'})
 _link1_2 = _link1_2.drop_duplicates()
+
+
+##########################
+# Step 2: Link by TICKER #
+##########################
+
+# Find links for the remaining unmatched cases using Exchange Ticker 
+
+# Identify remaining unmatched cases 
+_nomatch1 = pd.merge(_kld3[['companyname']], _link1_2[['permno','companyname']], on='companyname', how='left')
+_nomatch1 = _nomatch1.loc[_nomatch1.permno.isnull()].drop(['permno'], axis=1).drop_duplicates()
+
+# Add KLD identifying information
+kldid = _kld2
+kldid = kldid.loc[kldid.companyname.notna()]
+
+_nomatch2 = pd.merge(_nomatch1, kldid, how='inner', on=['companyname'])
+
+# Create first and last 'start dates' for Exchange Tickers
+# Label date range variables and keep only most recent company name
+
+_nomatch3 = _nomatch2.groupby(['companyname', 'ticker']).date.agg(['min', 'max'])\
+.reset_index().rename(columns={'min':'fdate', 'max':'ldate'})
+
+_nomatch3 = pd.merge(_nomatch2, _nomatch3, how='left', on=['companyname','ticker'])
+
+_nomatch3 = _nomatch3.loc[_nomatch3.date == _nomatch3.ldate]
+
+# Get entire list of CRSP stocks with Exchange Ticker information
+
+_crsp_n1 = conn.raw_sql(""" select ticker, comnam, permno, ncusip, namedt, nameenddt
+                            from crsp.stocknames """)
+
+_crsp_n1 = _crsp_n1.loc[_crsp_n1.ticker.notna()].sort_values(by=['permno','ticker','namedt'])
+
+# Arrange effective dates for link by Exchange Ticker
+
+_crsp_n1_namedt = _crsp_n1.groupby(['permno','ticker']).namedt.min().reset_index().rename(columns={'min':'namedt'})
+_crsp_n1_nameenddt = _crsp_n1.groupby(['permno','ticker']).nameenddt.max().reset_index().rename(columns={'max':'nameenddt'})
+
+_crsp_n1_dt = pd.merge(_crsp_n1_namedt, _crsp_n1_nameenddt, how = 'inner', on=['permno','ticker'])
+
+_crsp_n1 = _crsp_n1.rename(columns={'namedt': 'namedt_ind', 'nameenddt':'nameenddt_ind'})
+
+_crsp_n2 = pd.merge(_crsp_n1, _crsp_n1_dt, how ='left', on = ['permno','ticker'])
+
+_crsp_n2 = _crsp_n2.rename(columns={'ticker':'crsp_ticker'})
+_crsp_n2 = _crsp_n2.loc[_crsp_n2.nameenddt_ind == _crsp_n2.nameenddt].drop(['namedt_ind', 'nameenddt_ind'], axis=1)
+
+# Merge remaining unmatched cases using Exchange Ticker 
+# Note: Use ticker date ranges as exchange tickers are reused overtime
+
+_link2_1 = pd.merge(_nomatch3, _crsp_n2, how='inner', left_on=['ticker'], right_on=['crsp_ticker'])
+_link2_1 = _link2_1.loc[(_link2_1.ldate>=_link2_1.namedt) & (_link2_1.fdate<=_link2_1.nameenddt)]
+
+# Score using company name using 6-digit CUSIP and company name spelling distance
+_link2_1['name_ratio'] = _link2_1.apply(lambda x: fuzz.token_set_ratio(x.comnam, x.companyname), axis=1)
+
+_link2_2 = _link2_1
+_link2_2['cusip6'] = _link2_2[_link2_2['cusip'].notna()].apply(lambda x: x.cusip[:6], axis=1)
+_link2_2['ncusip6'] = _link2_2.apply(lambda x: x.ncusip[:6], axis=1)
+_link2_2['ncusip1_7'] = _link2_2.apply(lambda x: x.ncusip[1:7], axis=1)
+
+
+# Score using company name using 6-digit CUSIP and company name spelling distance
+
+def score2(row):
+    if ((row['cusip6']==row['ncusip6']) | (row['cusip6']==row['ncusip1_7'])) & (row['name_ratio'] >= name_ratio_p10):
+        score = 0
+    elif ((row['cusip6']==row['ncusip6']) | (row['cusip6']==row['ncusip1_7'])):
+        score = 4
+    elif row['name_ratio'] >= name_ratio_p10:
+        score = 5
+    else:
+        score = 6
+    return score
+
+# assign size portfolio
+_link2_2['score']=_link2_2.apply(score2, axis=1)
+
+# Some companies may have more than one TICKER-PERMNO link
+# so re-sort and keep the case (PERMNO & Company name from CRSP)
+# that gives the lowest score for each KLD TICKER 
+
+_link2_2 = _link2_2[['cusip','ticker','permno','companyname','comnam', 'name_ratio', 'score']].sort_values(by=['companyname','ticker','score'])
+_link2_2_score = _link2_2.groupby(['companyname', 'ticker']).score.min().reset_index()
+
+_link2_3 = pd.merge(_link2_2, _link2_2_score, how='inner', on=['companyname', 'ticker', 'score'])
+_link2_3 = _link2_3[['cusip','ticker','permno','companyname','comnam','name_ratio','score']].drop_duplicates()
+
+#####################################
+# Step 3: Finalize LInks and Scores #
+#####################################
+
+iclink = _link1_2.append(_link2_3)
